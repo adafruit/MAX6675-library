fd58049914c436b9c016a17ccbe203fc388793aa
lady ada <limor@ladyada.net>
2020-06-04 15:26:11 -0400

    remove unused vcc pins

--- examples/serialthermocouple/serialthermocouple.ino
+++ examples/serialthermocouple/serialthermocouple.ino
@@ -8,15 +8,10 @@ int thermoCS = 5;
 int thermoCLK = 6;
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
-int vccPin = 3;
-int gndPin = 2;
-  
+
 void setup() {
   Serial.begin(9600);
-  // use Arduino pins 
-  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
-  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
-  
+
   Serial.println("MAX6675 test");
   // wait for MAX chip to stabilize
   delay(500);
